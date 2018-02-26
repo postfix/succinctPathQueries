d87426f3a43196f1542c09623b46b474a2ce9ea2
Serikzhan <kazi.serikzhan@gmail.com>
2018-02-26 10:34:01 -0400

    added bp_supprt_gg

--- bp_tree.hpp
+++ bp_tree.hpp
@@ -3,6 +3,7 @@
 
 #include "succinct_tree.hpp"
 #include "sdsl/bp_support_sada.hpp"
+#include "sdsl/bp_support_gg.hpp"
 
 #include "sdsl/int_vector.hpp"
 #include "sdsl/rank_support.hpp"
@@ -20,15 +21,21 @@
 #include <algorithm>
 #endif
 #include <iostream>
+#define SADA 0
 
 class bp_tree: public succinct_tree {
 public:
 	typedef sdsl::bp_support_sada<> bp_support_sada;
+	typedef sdsl::bp_support_gg<> bp_support_gg;
 	typedef sdsl::bit_vector bit_vector;
 	typedef pq_types::node_type node_type;
 	typedef pq_types::size_type size_type;
 private:
+#if SADA
 	bp_support_sada bp_sada;
+#else
+	bp_support_gg bp_sada;
+#endif
 	bit_vector m_bv;
 	size_type _sz= 0;
 
@@ -61,13 +68,21 @@ public:
 		for ( auto i = 0; i < k; ++i )
 			if ( s[i] == '(' )
 				m_bv[i] = 1;
+#if SADA
 		bp_sada = bp_support_sada(&m_bv);
+#else
+		bp_sada = bp_support_gg(&m_bv);
+#endif
 		_sz+= sdsl::size_in_bytes(m_bv);
 	}
 	
 	bp_tree( const bit_vector *bp ) {
 		_sz= 0;
+#if SADA
 		bp_sada = bp_support_sada(bp);
+#else
+		bp_sada = bp_support_gg(bp);
+#endif
 		//_sz+= sdsl::size_in_bytes(*bp);
 		//bp_sada.set_vector(bp);
 	}
@@ -86,26 +101,33 @@ public:
 		auto pos = node2position(x);
 		if ( pos == 0 )
 			return 0;
-		assert( bp_sada.is_opening(pos) );
+		//assert( bp_sada.is_opening(pos) );
 		return position2node( bp_sada.enclose( node2position(x) ) );
 	}
 
 	node_type ancestor( const node_type x, const size_type i ) const {
 		if ( x == 0 ) return 0;
+#if SADA
 		return position2node( bp_sada.enclose(node2position(x),i) );
+#else
+		return position2node( bp_sada.enclose(node2position(x)) );
+#endif
 	}
 
 	std::vector<node_type> children( const node_type x ) const {
 		auto ix = interval(x);
 		std::vector<node_type> res;
 		//TODO: assert res.size() == 0;
+#if SADA
 		for ( auto i = ix.first; i+1 < bp_sada.size() && bp_sada.is_opening(i+1); i = bp_sada.find_close(i+1) ) {
 			//TODO: assert( is_opening(i+1) );
 			res.push_back(position2node(i+1));
 		}
+#endif
 		return res;
 	}
 
+	/*
 	node_type lca( const node_type x, const node_type y ) const {
 		if ( is_ancestor(x,y) )
 			return x;
@@ -120,6 +142,23 @@ public:
 			assert( !bp_sada.is_opening(irmq) );
 			auto ipre_lca = bp_sada.find_open(irmq);
 			return parent(position2node(ipre_lca));
+		}
+		swap(ix,iy);
+		goto label01;
+		assert( false );
+	}
+	*/
+	node_type lca( const node_type x, const node_type y ) const {
+		if ( is_ancestor(x,y) )
+			return x;
+		if ( is_ancestor(y,x) )
+			return y;
+		auto ix = interval(x),
+			 iy = interval(y);
+		assert( ix.second < iy.first || iy.second < ix.first );
+		if ( ix.second < iy.first ) {
+	label01:
+			return position2node(bp_sada.double_enclose(ix.first,iy.first));
 		}
 		swap(ix,iy);
 		goto label01;
